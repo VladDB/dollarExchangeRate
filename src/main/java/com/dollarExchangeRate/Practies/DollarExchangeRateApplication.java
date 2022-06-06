3630e313126c4614780e19bc585cdcca36dcb095
MacBook <VladDB@github.com>
2022-06-06 23:18:10 +0300

    init project

---
+++ src/main/java/com/dollarExchangeRate/Practies/DollarExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.dollarExchangeRate.Practies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dollarExchangeRate/Practies/DollarExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollarExchangeRate.Practies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
